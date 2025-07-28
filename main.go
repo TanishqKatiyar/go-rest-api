23b72c89d146a146e376c75ca30a5c1c613546c3
Tanishq Katiyar <2k22.cse.2213339@gmail.com>
2025-07-28 21:47:32 +0530

    Connect to Postgres

--- main.go
+++ main.go
@@ -20,3 +20,7 @@ func HandleRequest(w http.ResponseWriter, r *http.Request) {
   w.WriteHeader(http.StatusOK)
 }
 
+func HandleRequest(w http.ResponseWriter, r *http.Request) {
+  w.WriteHeader(http.StatusOK)
+}
+
