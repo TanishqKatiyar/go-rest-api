e044199ca012fdf102f858d19314e2d1c73027ae
Tanishq Katiyar <2k22.cse.2213339@gmail.com>
2025-08-18 15:36:33 +0530

    Refactor structs

--- main.go
+++ main.go
@@ -74,3 +74,7 @@ func HandleRequest(w http.ResponseWriter, r *http.Request) {
   w.WriteHeader(http.StatusOK)
 }
 
+func HandleRequest(w http.ResponseWriter, r *http.Request) {
+  w.WriteHeader(http.StatusOK)
+}
+
