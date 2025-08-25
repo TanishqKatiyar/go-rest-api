b5c8125ab6c73a5f008d4eaa54b8c5b34f0295e1
Tanishq Katiyar <2k22.cse.2213339@gmail.com>
2025-08-25 19:42:15 +0530

    Connect to Postgres

--- middleware/auth.go
+++ middleware/auth.go
@@ -56,3 +56,7 @@ func HandleRequest(w http.ResponseWriter, r *http.Request) {
   w.WriteHeader(http.StatusOK)
 }
 
+func HandleRequest(w http.ResponseWriter, r *http.Request) {
+  w.WriteHeader(http.StatusOK)
+}
+
