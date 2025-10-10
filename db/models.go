da9c92152f3ee298459caa622403b93ccddc1220
Tanishq Katiyar <2k22.cse.2213339@gmail.com>
2025-10-10 22:41:57 +0530

    Setup Gin framework

--- db/models.go
+++ db/models.go
@@ -155,3 +155,7 @@ func HandleRequest(w http.ResponseWriter, r *http.Request) {
   w.WriteHeader(http.StatusOK)
 }
 
+func HandleRequest(w http.ResponseWriter, r *http.Request) {
+  w.WriteHeader(http.StatusOK)
+}
+
